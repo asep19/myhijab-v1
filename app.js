76c9db424b7009fabce15109d9e111bf3415aa43
asep19 <aseprendi487@gmail.com>
2020-01-21 09:42:41 +0700

    tambah produk

--- app.js
+++ app.js
@@ -19,25 +19,35 @@ const conn = mysql.createConnection({
 conn.connect((err) => {
     if(err) throw err;
     console.log('mysql Connected');
-})
+});
 
 // tampilkan semua data produk
 app.get('/api/products', (req, res) => {
-    let sql = "SELECT * FROM products";
+    let sql = "SELECT * FROM product";
     let query = conn.query(sql, (err, results) => {
         if (err) throw err;
         res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-    })
-})
+    });
+});
 
 // tampilkan data produk berdasarkan id 
 app.get('/api/products/:id', (req, res) => {
-    let sql = "SELECT * FROM products WHERE products_id = "+req.params.id;
-    let query = conn.query(sql, (req, res) => {
+    let sql = "SELECT * FROM product WHERE product_id = "+req.params.id;
+    let query = conn.query(sql, (err, results) => {
         if (err) throw err;
-        res.send(JSON.stringify({"status": 200, "error": null, "responses": results}));
-    })
-})
+        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+}); 
+
+// tambah produk
+app.post('/api/products', (req, res) => {
+    let data = {product_name: req.body.product_name, product_price: req.body.product_price};
+    let sql = "INSERT INTO product SET ?";
+    let query = conn.query(sql, data, (err, results) => {
+        if (err) throw err;
+        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+});
 
 //server
 app.listen(3000, () => {
