634b6e39d12da5db50dc50791b9da4c2428bfdaa
asep19 <aseprendi487@gmail.com>
2020-01-21 13:49:24 +0700

    edit layout code

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const path = require('path');
 
 const app = express();
 
-// parse application/json
-app.use(bodyParser.json());
-
 // create database connection
 const conn = mysql.createConnection({
     host: 'localhost',
@@ -15,14 +12,15 @@ const conn = mysql.createConnection({
     password: '',
     database: 'myhijab'
 });
-
 // connect to database
 conn.connect((err) => {
     if(err) throw err;
     console.log('mysql Connected');
 });
 
-app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json()); // parse application/json
+
+app.set('views', path.join(__dirname, 'views')); // set views file
 
 // tampilkan semua data produk
 app.get('/api/products', (req, res) => {
