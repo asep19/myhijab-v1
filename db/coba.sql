8af491f4a7ecae67ef6eb4e6411dae194d3197b8
asep19 <aseprendi487@gmail.com>
2020-02-01 08:57:11 +0700

    update coba.sql

--- db/coba.sql
+++ db/coba.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 28, 2020 at 04:26 AM
+-- Generation Time: Feb 01, 2020 at 02:55 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -36,11 +36,98 @@ CREATE TABLE `brands` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `id_invoice` int(100) NOT NULL,
+  `id_orders` int(100) NOT NULL,
+  `id_status_invoice` int(100) NOT NULL,
+  `tanggal_invoice` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keranjang`
+--
+
+CREATE TABLE `keranjang` (
+  `id_keranjang` int(100) NOT NULL,
+  `id_pelanggan` int(100) NOT NULL,
+  `id_produk` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kurir`
+--
+
+CREATE TABLE `kurir` (
+  `id_kurir` int(100) NOT NULL,
+  `nama_kurir` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `metode_pembayaran`
+--
+
+CREATE TABLE `metode_pembayaran` (
+  `id_metode_pembayaran` int(100) NOT NULL,
+  `nama_pembayaran` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id_orders` int(100) NOT NULL,
+  `id_pelanggan` int(100) NOT NULL,
+  `id_status_order` int(100) NOT NULL,
+  `tanggal order` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_produk`
+--
+
+CREATE TABLE `orders_produk` (
+  `id_orders_produk` int(100) NOT NULL,
+  `id_produk` int(100) NOT NULL,
+  `id_orders` int(100) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `harga` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(100) NOT NULL,
+  `nama_pelanggan` varchar(255) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `produk`
 --
 
 CREATE TABLE `produk` (
-  `Id_produk` int(100) NOT NULL,
+  `id_produk` int(100) NOT NULL,
   `nama_produk` varchar(255) NOT NULL,
   `harga_produk` int(100) NOT NULL,
   `jumlah` int(100) NOT NULL,
@@ -54,7 +141,7 @@ CREATE TABLE `produk` (
 --
 
 CREATE TABLE `produk_detail` (
-  `Id_produk` int(100) NOT NULL,
+  `id_produk` int(100) NOT NULL,
   `deskripsi` varchar(255) NOT NULL,
   `review` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -70,6 +157,39 @@ CREATE TABLE `produk_kategori` (
   `nama_kategori` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status_invoice`
+--
+
+CREATE TABLE `status_invoice` (
+  `id_status_invoice` int(100) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status_orders`
+--
+
+CREATE TABLE `status_orders` (
+  `id_status_orders` int(100) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlist`
+--
+
+CREATE TABLE `wishlist` (
+  `id_pelanggan` int(100) NOT NULL,
+  `id_produk` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
@@ -81,22 +201,91 @@ ALTER TABLE `brands`
   ADD PRIMARY KEY (`id_brands`);
 
 --
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`id_invoice`),
+  ADD KEY `id_orders` (`id_orders`),
+  ADD KEY `id_status_invoice` (`id_status_invoice`);
+
+--
+-- Indexes for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD PRIMARY KEY (`id_keranjang`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`),
+  ADD KEY `id_produk` (`id_produk`);
+
+--
+-- Indexes for table `kurir`
+--
+ALTER TABLE `kurir`
+  ADD PRIMARY KEY (`id_kurir`);
+
+--
+-- Indexes for table `metode_pembayaran`
+--
+ALTER TABLE `metode_pembayaran`
+  ADD PRIMARY KEY (`id_metode_pembayaran`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id_orders`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`),
+  ADD KEY `id_status_order` (`id_status_order`);
+
+--
+-- Indexes for table `orders_produk`
+--
+ALTER TABLE `orders_produk`
+  ADD PRIMARY KEY (`id_orders_produk`),
+  ADD KEY `id_produk` (`id_produk`),
+  ADD KEY `id_orders` (`id_orders`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
+
+--
 -- Indexes for table `produk`
 --
 ALTER TABLE `produk`
-  ADD PRIMARY KEY (`Id_produk`);
+  ADD PRIMARY KEY (`id_produk`);
 
 --
 -- Indexes for table `produk_detail`
 --
 ALTER TABLE `produk_detail`
-  ADD KEY `Id_produk` (`Id_produk`);
+  ADD KEY `Id_produk` (`id_produk`);
 
 --
 -- Indexes for table `produk_kategori`
 --
 ALTER TABLE `produk_kategori`
   ADD PRIMARY KEY (`id_kategori_produk`);
+
+--
+-- Indexes for table `status_invoice`
+--
+ALTER TABLE `status_invoice`
+  ADD PRIMARY KEY (`id_status_invoice`);
+
+--
+-- Indexes for table `status_orders`
+--
+ALTER TABLE `status_orders`
+  ADD PRIMARY KEY (`id_status_orders`);
+
+--
+-- Indexes for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD KEY `id_pelanggan` (`id_pelanggan`),
+  ADD KEY `id_produk` (`id_produk`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -109,16 +298,70 @@ ALTER TABLE `brands`
   MODIFY `id_brands` int(100) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `id_invoice` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  MODIFY `id_keranjang` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kurir`
+--
+ALTER TABLE `kurir`
+  MODIFY `id_kurir` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `metode_pembayaran`
+--
+ALTER TABLE `metode_pembayaran`
+  MODIFY `id_metode_pembayaran` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id_orders` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders_produk`
+--
+ALTER TABLE `orders_produk`
+  MODIFY `id_orders_produk` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id_pelanggan` int(100) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `Id_produk` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produk` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `produk_kategori`
 --
 ALTER TABLE `produk_kategori`
   MODIFY `id_kategori_produk` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `status_invoice`
+--
+ALTER TABLE `status_invoice`
+  MODIFY `id_status_invoice` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `status_orders`
+--
+ALTER TABLE `status_orders`
+  MODIFY `id_status_orders` int(100) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
